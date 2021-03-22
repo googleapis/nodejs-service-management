30c725c248250160d4b579ac430a869e2589d113
Sofia Leon <sofialeon@google.com>
2021-03-22 16:38:36 -0700

    remove Node 10 tests

--- synth.py
+++ synth.py
@@ -30,12 +30,12 @@ for version in versions:
   name, 
   version,
   bazel_target=f"//google/api/servicemanagement/{version}:api-servicemanagement-{version}-nodejs")
-  s.copy(library, excludes=["package.json","linkinator.config.json"])
+  s.copy(library, excludes=["package.json","README.md"])
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(
     source_location='build/src', versions=["v1"], default_version="v1")
-s.copy(templates, excludes=[])
+s.copy(templates, excludes=[".github/workflows/ci.yaml"])
 
 node.postprocess_gapic_library()
